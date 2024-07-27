a1bf4f7d45800b7b2198099b164bb6f7f4985890
Patrick Domingo <patrickleinieldomingo@gmail.com>
2024-07-27 21:50:30 +0800

    - prepare global state

--- ArchitectureMentoringSession/ArchitectureMentoringSession/Domain/GlobalState.swift
+++ ArchitectureMentoringSession/ArchitectureMentoringSession/Domain/GlobalState.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+typealias UserID = ModelID<User>
+typealias PostID = ModelID<Post>
+typealias CategoryID = ModelID<Category>
+
+struct GlobalState {
+    
+    // MARK: - Models
+    var userIds = [UserID]()
+    var userById = [UserID: User]()
+    
+    var postIds = [PostID]()
+    var postById = [PostID: Post]()
+    
+    var categoryIds = [CategoryID]()
+    var categoryById = [CategoryID: Category]()
+    
+    var itemById = [ItemID: MarketItem]()
+    
+    // MARK: - Relationships
+    var postIdsByUserId = [UserID: [PostID]]()
+    var itemIdsByUserId = [UserID: [MarketItem]]()
+    var postIdsByCategoryId = [CategoryID: [PostID]]()
+}
